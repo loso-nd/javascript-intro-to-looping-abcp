bef25270eb7db625e740505811be1db2cd948e7d
Nahomie <nducasse3@gmail.com>
2018-01-17 15:22:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,8 @@
 for (let i = 0; i < 25; i++){
-
- console.log("I am " + i + " strange loop.");
+if( i === 0){
+  console.log("I am 1 strange loop.");
+} else {
+  console.log("I am " + i + " strange loop.");
+  
+}
 }
