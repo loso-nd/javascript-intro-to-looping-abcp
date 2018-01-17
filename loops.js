ab696dd93c21256a557a093d11142cfa862ac54b
Nahomie <nducasse3@gmail.com>
2018-01-17 15:00:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array){
-  for (let i = 0; i < 5; i++){
+  for (let i = 0; i < 25; i++){
   //return (`I am 1 strange loop ? I am + ${i} + strange loops.`
   if(i === 1 ){
  console.log("I am 1 strange loop.");
