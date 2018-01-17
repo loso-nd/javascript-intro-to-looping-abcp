52e59a63c4c28d75c899b67c947e63c27064d2e5
Nahomie <nducasse3@gmail.com>
2018-01-17 13:44:20 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    i === 1 ? "I am 1 strange loop" : "I am " + array[i] + "strange loops."
+  //  if(i == 1 ?){
+  //  console.log("I am 1 strange loop.");
+  //} else {
+    //  return `I am ${i} strange loops.`
+  //}
+  }
+}
