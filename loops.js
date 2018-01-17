f724e9c12a105aa205408b873a45ab2534b46b61
Nahomie <nducasse3@gmail.com>
2018-01-17 14:58:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
-  for (var i = 0; i < 25; i++){
-    //i === 0 ? `I am 1 strange loop : I am + ${i} + strange loops.`
-    if(i === 1 ){
-   console.log("I am 1 strange loop.");
-  } else {
-     console.log("I am " + ${i} + "strange loops.");
-    }
+  for (var i = 0; i < 5; i++){
+  //return (`I am 1 strange loop ? I am + ${i} + strange loops.`
+  //  if(i === 1 ){
+ console.log("I am 1 strange loop.");
+//  } else {
+  //   console.log("I am " + ${i} + "strange loops.");
+  //  }
   }
 }
